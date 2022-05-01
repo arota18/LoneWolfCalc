c59dfdd0f898e509653bc5add9da521a62acf193
arota18 <a.rota@hotmail.com>
2022-05-01 19:46:40 +0200

    begining of work

--- app/src/main/java/it/andrea/lonewolfcalculator/MainActivity.java
+++ app/src/main/java/it/andrea/lonewolfcalculator/MainActivity.java
@@ -3,6 +3,8 @@ package it.andrea.lonewolfcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addEndurance(View view) {
+        this.editTxtNumber(view, false, true);
+    }
+
+    public void subEndurance(View view) {
+        this.editTxtNumber(view, false, false);
+    }
+
+    public void addCombat(View view) {
+        this.editTxtNumber(view, true, true);
+    }
+
+    public void subCombat(View view) {
+        this.editTxtNumber(view, true, false);
+    }
+
+    // private Methods
+
+    private void editTxtNumber(View view, boolean isEditCombat, boolean isAdd) {
+        EditText et;
+        if (isEditCombat)
+            et = findViewById(R.id.txtCombat);
+        else
+            et = findViewById(R.id.txtEndurance);
+        int val = -1;
+        try {
+            val = Integer.parseInt(et.getText().toString());
+        } catch (Exception e) {
+            if (isAdd)
+                val = 0;
+        }
+        if (val != -1) {
+            if (isAdd)
+                val++;
+            else
+                val--;
+            et.setText(String.valueOf(val));
+        }
     }
 }
