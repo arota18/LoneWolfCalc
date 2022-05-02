99a4b48ca85c0d0a5580ba514caac13c7ccd0764
arota18 <79203913+a-rota@users.noreply.github.com>
2022-05-02 11:37:13 +0200

    wip test catching extra value

--- app/src/main/java/it/andrea/lonewolfcalculator/FightActivity.java
+++ app/src/main/java/it/andrea/lonewolfcalculator/FightActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FightActivity extends AppCompatActivity {
+
+    private int lwEndurance;
+    private int lwCombat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fight);
-//        int lwEndurance = getIntent().getIntExtra(MainActivity.EXT_ENDURANCE);
-
+        Bundle ext = getIntent().getExtras();
+        if (ext != null) {
+            lwEndurance = ext.getInt(MainActivity.EXT_ENDURANCE);
+            lwCombat = ext.getInt(MainActivity.EXT_COMBAT);
+        }
+        ((TextView)findViewById(R.id.textView)).setText(String.valueOf(lwEndurance) + " " + String.valueOf(lwCombat));
     }
 }
