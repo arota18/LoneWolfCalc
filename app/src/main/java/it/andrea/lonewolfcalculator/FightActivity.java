3224b7b129ffa1d1c25f0c999553b5955805a33c
arota18 <104698199+arota18@users.noreply.github.com>
2022-05-05 23:54:49 +0200

    wip combat calc

--- app/src/main/java/it/andrea/lonewolfcalculator/FightActivity.java
+++ app/src/main/java/it/andrea/lonewolfcalculator/FightActivity.java
@@ -2,11 +2,12 @@ package it.andrea.lonewolfcalculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FightActivity extends TwoCounterActivity {
 
-    private int lwEndurance;
-    private int lwCombat;
+    private int lwEndurance, lwCombat, enEndurance, combatRateo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,15 @@ public class FightActivity extends TwoCounterActivity {
 
     // buttons handlings
 
+    public void startFight(View v) {
+        enEndurance = Integer.parseInt(etEndurance.getText().toString());
+        int enCombat = Integer.parseInt(etCombat.getText().toString());
+        if (enEndurance > 0 && enCombat > 0) {
+            combatRateo = lwCombat - enCombat;
+            ((TextView) findViewById(R.id.textView3)).setText(fullCombat());
+        }
+    }
+
     public void subEnemyEndurance(View v) {
         this.editTxtValue(v, 1);
     }
@@ -37,5 +47,45 @@ public class FightActivity extends TwoCounterActivity {
 
     public void addEnemyCombat(View v) {
         this.editTxtValue(v, 4);
+    }
+
+    private String fullCombat() {
+        return fullCombat("", 0);
+    }
+
+    private String fullCombat(String s, int i) {
+        i++;
+        final int roll = dice10();
+        lwEndurance -= lwReceivedDmg(roll);
+        enEndurance -= enReceivedDmg(roll);
+        // todo: exit condition, just for testing
+        if (lwEndurance < 1 || enEndurance < 1) return i + " round, " + s;
+        return fullCombat(s, i);
+    }
+
+    private static int dice10() {
+        // min + random*(max - min + 1)
+        return (int) Math.floor(1 + Math.random() * 10);
+    }
+
+    private int lwReceivedDmg(int roll) {
+        switch (roll) {
+            case 0:
+                return 0;
+            case 1:
+                if (combatRateo > 8) return 3;
+                else if (combatRateo > 2) return 4;
+                else if (combatRateo > -3) return 5;
+                else if (combatRateo > -7) return 6;
+                else if (combatRateo > -9) return 8;
+                else return lwEndurance;
+        }
+        // todo: switch from LW table
+        return -1;
+    }
+
+    private int enReceivedDmg(int roll) {
+        // todo: switch from LW table
+        return -1;
     }
 }
